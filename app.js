ba3c6796484c2a72f0247aa92571c5a6b1a90282
Niraj Kumar <kumarniraj8607@gmail.com>
2025-08-31 03:49:57 +0530

    Add Poject file

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ store.on("error",()=>{
 });
 const sessionOptions = {
   store,
-  secret: SECRET,
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: { expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true },
